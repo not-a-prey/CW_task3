5a2d86de2dd4256542d7734894eb68ab82afab21
not_a_prey <119671453+not-a-prey@users.noreply.github.com>
2024-10-29 20:13:53 +0000

    Update top_script.sh
    Added description of the script

--- top_script.sh
+++ top_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#The script executes top command with num parameter indicating the number of
+#lines to be written to the file. If no parameter provided, the value 5 is used.
+
 num=${1:-5}
 
 top -b -n 1 | head -n "$num" > top_script_output.txt
